41cb4da6468c678badc59e74481fe9a15f11dc68
Viviannwadialu <alajembaviviannkem@gmail.com>
2023-01-03 12:37:37 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var enovtech = "welcome to enovtech"
+enovtech = "second enovtech " + enovtech;
+console.log(enovtech);
+var age = 23;
+console.log(age);
+var married = false;
+console.log(married);
+var nothing;
+console.log(nothing);
+nothing = age;
+console.log(nothing);
+var details = {
+    name: "vivian",
+    age: 23,
+    married: false,
+};
+console.log(details);
+console.log(typeof details);
+console.log(typeof age);
+console.log(typeof nothing);
+console.log(typeof married);
